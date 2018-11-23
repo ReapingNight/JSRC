ff0176c35b3591af5d046f7e509481d551d3c06a
ReapingNight <christiaanbotha22@gmail.com>
2018-11-23 17:27:54 +0100

    Started implementing game pieces

---
+++ JSRC/app/public/javascripts/gamePiece.js
@@ -0,0 +1,13 @@
+var piece = new Object()
+{
+    color;
+    type;
+    position;
+}
+
+function piece(color, type, position)
+{
+    this.color = color;
+    this.type = type;
+    this.position = position;
+}
