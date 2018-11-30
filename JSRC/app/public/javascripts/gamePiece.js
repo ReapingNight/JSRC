8a4443ddda4444a7ea36e328d8d003fab9b977e5
ReapingNight <christiaanbotha22@gmail.com>
2018-11-30 14:18:35 +0100

    Continued with gamePiece.js and game.css

--- JSRC/app/public/javascripts/gamePiece.js
+++ JSRC/app/public/javascripts/gamePiece.js
@@ -1,7 +1,7 @@
 var piece = new Object()
 {
-    color;
-    type;
+    color;                  //0 - Black; 1 - White
+    type;                   //0 - Pawn; 1 - Rook; 2 - Knight; 3 - Bishop; 4 - Queen; 5 - King
     position;
 }
 
@@ -11,3 +11,8 @@ function piece(color, type, position)
     this.type = type;
     this.position = position;
 }
+
+piece.prototype.getPosition = function(){return this.position;};
+piece.prototype.setPosition = function(pos){this.position = pos;};
+piece.prototype.getColor = function(){return this.color;};
+piece.prototype.getType = function(){return this.type;};
