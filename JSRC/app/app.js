02645ede50a01bbc746a38a408c5b6da32d9a46a
ReapingNight <christiaanbotha22@gmail.com>
2018-12-10 11:38:40 +0100

    Added modules for game and board

--- JSRC/app/app.js
+++ JSRC/app/app.js
@@ -11,42 +11,7 @@ var settings;
 app.use(express.static(__dirname + "/public"));
 var server = http.createServer(app);
 
-let mover = require("piece-mover");
-var board = [
-        {color: 0, type:1, position: 63},
-        {color: 0, type:2, position: 62},
-        {color: 0, type:3, position: 61},
-        {color: 0, type:5, position: 60},
-        {color: 0, type:4, position: 59},
-        {color: 0, type:3, position: 58},
-        {color: 0, type:2, position: 57},
-        {color: 0, type:1, position: 56},
-        {color: 0, type:0, position: 55},
-        {color: 0, type:0, position: 54},
-        {color: 0, type:0, position: 53},
-        {color: 0, type:0, position: 52},
-        {color: 0, type:0, position: 51},
-        {color: 0, type:0, position: 50},
-        {color: 0, type:0, position: 49},
-        {color: 0, type:0, position: 48},
-
-        {color: 1, type:1, position: 0},
-        {color: 1, type:2, position: 1},
-        {color: 1, type:3, position: 2},
-        {color: 1, type:4, position: 3},
-        {color: 1, type:5, position: 4},
-        {color: 1, type:3, position: 5},
-        {color: 1, type:2, position: 6},
-        {color: 1, type:1, position: 7},
-        {color: 1, type:0, position: 8},
-        {color: 1, type:0, position: 9},
-        {color: 1, type:0, position: 10},
-        {color: 1, type:0, position: 11},
-        {color: 1, type:0, position: 12},
-        {color: 1, type:0, position: 13},
-        {color: 1, type:0, position: 14},
-        {color: 1, type:0, position: 15}
-]
+var board = require("chess-board");
 
 var playerCount = 0;
 
@@ -55,7 +20,7 @@ app.get("/", indexRouter);
 
 //Route for sending starting json with all the pieces in place
 app.get("/start", function(req, res){
-        res.json(board);
+        res.json(board.board);
 });
 
 //Catch any stray url paths
@@ -77,7 +42,7 @@ wss.on("connection", function(ws) {
                 {
                         case "MOVE":
                                 let positions = words[1].split(",");
-                                let temp = mover.move(board, positions[0], positions[1]);
+                                let temp = board.move(positions[0], positions[1]);
                                 if(temp !== null)
                                 {
                                         ws.send("MAKE_MOVE " + JSON.stringify(temp));
